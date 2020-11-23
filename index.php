4e9b8d2ead566e30bf0b95bbb78b0d24db22696d
pinkevycdima@gmail.com <pinkevycdima@gmail.com>
2020-11-23 18:00:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include "phpFiles/_head.php"; ?>
+
+<body>
+
+    <?php include "phpFiles/_header.php"; ?>
+
+    <?php include "phpFiles/hero.php"; ?>
+
+    <?php include "phpFiles/main.php"; ?>
+
+    <?php include "phpFiles/footer.php"; ?>
+
+    <?php include "phpFiles/scripts.php"; ?>
+
+</body>
+
+</html>
---
+++ phpFiles/main.php
@@ -0,0 +1,21 @@
+<main id="main">
+
+    <?php include "about.php"; ?>
+
+    <?php include "count.php"; ?>
+
+    <?php include "services.php"; ?>
+
+    <?php include "features.php"; ?>
+
+    <?php include "test_sec.php"; ?>
+
+    <?php include "portfolio.php"; ?>
+
+    <?php include "pricing.php"; ?>
+
+    <?php include "questions.php"; ?>
+
+    <?php include "contact.php"; ?>
+
+</main><!-- End #main -->
